d1e4648eb91c8b6d8f6ea639b148c1d6424690d9
Andile <andile.goba@younglings.africa>
2022-11-18 13:30:08 +0200

    NFTCard file update commit

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -8,65 +8,65 @@ import { RectButton, CircleButton } from './Button';
 
 const NFTCard = ({ data }) => {
     const navigation = useNavigation();
-
-    return(
+  
+    return (
+      <View
+        style={{
+          backgroundColor: COLORS.white,
+          borderRadius: SIZES.font,
+          marginBottom: SIZES.extraLarge,
+          margin: SIZES.base,
+          ...SHADOWS.dark,
+        }}
+      >
         <View
+          style={{
+            width: "100%",
+            height: 250,
+          }}
+        >
+          <Image
+            source={data.image}
+            resizeMode="cover"
             style={{
-                backgrounColor: COLORS.white,
-                borderRadius: SIZES.font,
-                marginBottom: SIZES.extraLarge,
-                margin: SIZES.base,
-                ...SHADOWS.dark,
+              width: "100%",
+              height: "100%",
+              borderTopLeftRadius: SIZES.font,
+              borderTopRightRadius: SIZES.font,
             }}
-        >  
-            <View
-                style={{
-                    width: "100%",
-                    height: 250,
-                }}
-            >
-                <Image 
-                    source={data.image}
-                    resizeMode="cover"
-                    style={{
-                        width: '100%',
-                        height: '100%',
-                        borderTopLeftRadius: SIZES.font,
-                        borderTopRightRadius: SIZES.font,
-                    }}
-                />
-
-                <CircleButton imgUrl={assets.heart} right={10} top={10} />
-            </View>
-
-            <SubInfo />
-
-            <View style={{ width: '100%', padding: SIZES.font}}>
-                <NFTTilte 
-                    title={data.name}
-                    subTitle={data.creator}
-                    titleSize={SIZES.large}
-                    subTitleSize={SIZES.small}
-                />
-
-                <View
-                    style={{
-                        marginTop: SIZES.font,
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                    }}
-                >
-                    <EthPrice price={data.price} />
-                    <RectButton 
-                        mioWidth={100}
-                        fontSize={SIZES.font}
-                        handlePress={() => navigation.navigate("Details", { data })}
-                    />
-                </View>
-            </View>
+          />
+  
+          <CircleButton imgUrl={assets.heart} right={10} top={10} />
         </View>
+  
+        <SubInfo />
+  
+        <View style={{ width: "100%", padding: SIZES.font }}>
+          <NFTTitle
+            title={data.name}
+            subTitle={data.creator}
+            titleSize={SIZES.large}
+            subTitleSize={SIZES.small}
+          />
+  
+          <View
+            style={{
+              marginTop: SIZES.font,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <EthPrice price={data.price} />
+            <RectButton
+              minWidth={120}
+              fontSize={SIZES.font}
+              handlePress={() => navigation.navigate("Details", { data })}
+            />
+          </View>
+        </View>
+      </View>
     );
-};
-
-export default NFTCard;
+  };
+  
+  export default NFTCard;

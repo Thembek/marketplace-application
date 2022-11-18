c22fac843489c7fabb4fd99074e78c99ee0d021e
Andile <andile.goba@younglings.africa>
2022-11-18 13:39:22 +0200

    found Text component error in Button js fie commit

--- components/Button.js
+++ components/Button.js
@@ -3,43 +3,52 @@ import { TouchableOpacity, Text, Image } from 'react-native';
 import { COLORS, SIZES, FONTS, SHADOWS } from '../constants';
 
 export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
-    return(
-        <TouchableOpacity
-            style={{
-                width: 40,
-                height: 40,
-                backgroundColor: COLORS.white,
-                position: 'absolute',
-                borderRadius: SIZES.extraLarge,
-                alignItems: 'center',
-                justifyContent: 'center',
-                ...SHADOWS.light,
-                ...props,
-            }}
-            onPress={handlePress}
+    return (
+      <TouchableOpacity
+        style={{
+          width: 40,
+          height: 40,
+          backgroundColor: COLORS.white,
+          position: "absolute",
+          borderRadius: SIZES.extraLarge,
+          alignItems: "center",
+          justifyContent: "center",
+          ...SHADOWS.light,
+          ...props,
+        }}
+        onPress={handlePress}
+      >
+        <Image
+          source={imgUrl}
+          resizeMode="contain"
+          style={{ width: 24, height: 24 }}
+        />
+      </TouchableOpacity>
+    );
+  };
+  
+  export const RectButton = ({ minWidth, fontSize, handlePress, ...props }) => {
+    return (
+      <TouchableOpacity
+        style={{
+          backgroundColor: COLORS.primary,
+          padding: SIZES.small,
+          borderRadius: SIZES.extraLarge,
+          minWidth: minWidth,
+          ...props,
+        }}
+        onPress={handlePress}
+      >
+        <Text
+          style={{
+            fontFamily: FONTS.semiBold,
+            fontSize: fontSize,
+            color: COLORS.white,
+            textAlign: "center",
+          }}
         >
-            <Image 
-                source={imgUrl}
-                resizeMode='contain'
-                style={{ width: 24, height: 24 }}
-            />
-        </TouchableOpacity>
+          Place a bid
+        </Text>
+      </TouchableOpacity>
     );
-};
-
-export const RectButton = ({ minWidth, fontSize, handlePress, ...props }) => {
-    return(
-        <TouchableOpacity
-            style={{
-                backgroundColor: COLORS.primary,
-                padding: SIZES.small,
-                borderRadius: SIZES.extraLarge,
-                minWIdth: minWidth,
-                ...props,
-            }}
-            onPress={handlePress}
-        >   
-            Place a bid
-        </TouchableOpacity>
-    );
-};
+  };
